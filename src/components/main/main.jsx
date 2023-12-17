012aaf98c9ee7c48f6629df5b1a136b2a1a13658
DESKTOP-MF8V5D1\Sergei <m>
2023-12-17 22:05:52 +0400

    Single Page Apps for GitHub Pages 404 issue solution

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<Router>
+		<Router basename="/react-redux-skills">
 			<Provider store={store}>
 				<App />
 			</Provider>
